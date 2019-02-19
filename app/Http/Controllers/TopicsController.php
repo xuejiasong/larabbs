4b1cc74cf7654e82a774cd8012207e990b1ee28e
vagrant <vagrant@homestead>
2019-02-19 06:31:06 +0000

    回复列表

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -45,7 +45,7 @@ class TopicsController extends Controller
         $topic->user_id = Auth::id();
         $topic->save();
 
-        return redirect()->route($topic->link())->with('success', '帖子创建成功！');
+        return redirect()->to($topic->link())->with('success', '帖子创建成功！');
     }
 
 	public function edit(Topic $topic)
@@ -60,7 +60,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-        return redirect()->route($topic->link())->with('success', '更新成功！');
+        return redirect()->to($topic->link())->with('success', '更新成功！');
 	}
 
 	public function destroy(Topic $topic)
