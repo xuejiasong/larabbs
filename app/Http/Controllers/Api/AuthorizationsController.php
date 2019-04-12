69b4a9a39e4da15f97aa35d1ccef25ce74d9202f
vagrant <vagrant@homestead>
2019-04-12 02:22:58 +0000

    add weixin

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -2,45 +2,48 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Requests\Api\SocialAuthorizationRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\SocialAuthorizationRequest;
 
 class AuthorizationsController extends Controller
 {
-    //
-    public function socialStore($type,SocialAuthorizationRequest $request){
-        if(!in_array($type,['weixin'])){
+    public function socialStore($type, SocialAuthorizationRequest $request)
+    {
+        if (!in_array($type, ['weixin'])) {
             return $this->response->errorBadRequest();
         }
+
         $driver = \Socialite::driver($type);
 
-        try{
-            if($code = $request->code){
+        try {
+            if ($code = $request->code) {
                 $response = $driver->getAccessTokenResponse($code);
-                $token = array_get($response,'access_token');
-            }else{
+                $token = array_get($response, 'access_token');
+            } else {
                 $token = $request->access_token;
-                if($type == 'weixin'){
+
+                if ($type == 'weixin') {
                     $driver->setOpenId($request->openid);
                 }
             }
+
             $oauthUser = $driver->userFromToken($token);
         } catch (\Exception $e) {
             return $this->response->errorUnauthorized('参数错误，未获取用户信息');
         }
 
         switch ($type) {
-            case 'weixin' :
+            case 'weixin':
                 $unionid = $oauthUser->offsetExists('unionid') ? $oauthUser->offsetGet('unionid') : null;
 
-                if($unionid) {
+                if ($unionid) {
                     $user = User::where('weixin_unionid', $unionid)->first();
-                }else{
+                } else {
                     $user = User::where('weixin_openid', $oauthUser->getId())->first();
                 }
 
-                //没有用户，默认创建一个用户
+                // 没有用户，默认创建一个用户
                 if (!$user) {
                     $user = User::create([
                         'name' => $oauthUser->getNickname(),
@@ -52,6 +55,7 @@ class AuthorizationsController extends Controller
 
                 break;
         }
+
         return $this->response->array(['token' => $user->id]);
     }
 }
