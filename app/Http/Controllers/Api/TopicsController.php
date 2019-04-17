1b536ed85425293e3d31c835cfb73da6e52d77b8
vagrant <vagrant@homestead>
2019-04-17 01:18:07 +0000

    topics index

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -4,12 +4,39 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\TopicRequest;
 use App\Models\Topic;
+use App\Models\User;
 use App\Transformers\TopicTransformer;
+use function GuzzleHttp\default_ca_bundle;
 use Illuminate\Http\Request;
 
 class TopicsController extends Controller
 {
     //
+    public function index(Request $request,Topic $topic)
+    {
+        $query = $topic->query();
+
+        if($categoryId = $request->category_id)
+        {
+            $query->where('category_id', $categoryId);
+        }
+
+        //为了说明 N+1问题，不适用scopWithOrder
+        switch ($request->order){
+            //scop 排序方式
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        $topics = $query->paginate(20);
+
+        return $this->response->paginator($topics, new TopicTransformer());
+    }
+
     public function store(TopicRequest $request,Topic $topic)
     {
         $topic->fill($request->all());
@@ -33,5 +60,12 @@ class TopicsController extends Controller
         $this->authorize('destroy',$topic);
         $topic->delete();
         return $this->response->noContent();
+    }
+
+    public function userIndex(User $user,Request $request)
+    {
+        $topics = $user->topics()->recent()
+            ->paginate(20);
+        return $this->response->paginator($topics, new TopicTransformer());
     }
 }
