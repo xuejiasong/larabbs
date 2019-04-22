10e437b43d7e5ce91bdb2d7166d3f0d444d7c5d5
vagrant <vagrant@homestead>
2019-04-22 06:49:47 +0000

    notifications index

---
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Transformers\NotificationTranformer;
+use Illuminate\Http\Request;
+
+
+class NotificationsController extends Controller
+{
+    //
+    public function index()
+    {
+        $notifications = $this->user->notifications()->paginate(20);
+
+        return $this->response->paginator($notifications, new NotificationTranformer());
+    }
+}
