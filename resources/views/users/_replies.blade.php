d0498b204e96d082ec1bf280ea87f774cd122aaa
vagrant <vagrant@homestead>
2019-04-26 08:51:46 +0000

    add easywechat

--- resources/views/users/_replies.blade.php
+++ resources/views/users/_replies.blade.php
@@ -1,5 +1,4 @@
 @if (count($replies))
-
     <ul class="list-group mt-4 border-0">
         @foreach ($replies as $reply)
             <li class="list-group-item pl-2 pr-2 border-right-0 border-left-0 @if($loop->first) border-top-0 @endif">
