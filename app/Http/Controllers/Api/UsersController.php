8339acf9ef545017684391746cf2e6e3d3e3d579
vagrant <vagrant@homestead>
2019-04-28 06:32:48 +0000

    weapp users store

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -26,6 +26,57 @@ class UsersController extends Controller
         return $this->response->item($user, new UserTransformer());
     }
 
+    public function weappStore(UserRequest $request)
+    {
+        //缓存中是否存在对应的key
+        $verifyData = \Cache::get($request->verification_key);
+
+        if (!$verifyData) {
+            return $this->response->error('验证码已失效', 422);
+        }
+
+        //判断验证码是否相等，不相等返回401错误
+        if (!hash_equals((string)$verifyData['code'], $request->verification_code)) {
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        //获取微信的openid 和 session_key
+        $miniProgram = \EasyWechat::miniProgram();
+        $data = $miniProgram->auth->session($request->code);
+
+        if (isset($data['errcode'])) {
+            return $this->response->errorUnauthorized('code 不正确');
+        }
+
+        //如果openid 对应的用户已存在，报错403
+        $user = User::where('weapp_openid', $data['openid'])->first();
+
+        if ($user) {
+            return $this->response->errorUnauthorized('微信已绑定其他用户，请直接登录');
+        }
+
+        //创建用户
+        $user = User::create([
+            'name' => $request->name,
+            'phone' => $verifyData['phone'],
+            'password' => bcrypt($request->password),
+            'weapp_openid' => $data['openid'],
+            'weixin_session_key' => $data['session_key'],
+        ]);
+
+        //清除验证码缓存
+        \Cache::forget($request->verification_key);
+
+        //meta中返回Token信息
+        return $this->response->item($user, new UserTransformer())
+            ->setMeta([
+                'access_token' => \Auth::guard('api')->fromUser($user),
+                'token_type' => 'Bearer',
+                'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+            ])
+            ->setStatusCode(201);
+    }
+
     public function store(UserRequest $request)
     {
         $verifyData = \Cache::get($request->verification_key);
--- routes/api.php
+++ routes/api.php
@@ -5,109 +5,108 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api',
-    'middleware' =>  ['serializer:array','bindings','change-locale']
+  'namespace' => 'App\Http\Controllers\Api',
+  'middleware' =>  ['serializer:array','bindings','change-locale']
 ], function($api) {
  $api->group([
-     'middleware' => 'api.throttle',
-     'limit' => config('api.rate_limits.sign.limit'),
-     'expires' => config('api.rate_limits.sign.expires'),
+   'middleware' => 'api.throttle',
+   'limit' => config('api.rate_limits.sign.limit'),
+   'expires' => config('api.rate_limits.sign.expires'),
  ],function ($api){
        // 短信验证码
-     $api->post('verificationCodes', 'VerificationCodesController@store')
-     ->name('api.verificationCodes.store');
+   $api->post('verificationCodes', 'VerificationCodesController@store')
+   ->name('api.verificationCodes.store');
        //用户注册
-     $api->post('users','UsersController@store')
-     ->name('api.users.store');
+   $api->post('users','UsersController@store')
+   ->name('api.users.store');
        //图片验证码
-     $api->post('captchas','CaptchasController@store')
-     ->name('api.captchas.store');
+   $api->post('captchas','CaptchasController@store')
+   ->name('api.captchas.store');
        //        //第三方登录
        //$api->post('socials/{social_type}/authorizations','AuthorizationsController@socialStore')
        //->name('api.socials.authorizations.store');
        // 第三方登录
-     $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
-     ->name('api.socials.authorizations.store');
+   $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
+   ->name('api.socials.authorizations.store');
        //登录
-     $api->post('authorizations','AuthorizationsController@store')
-     ->name('api.authorization.store');
-     $api->post('weapp/authorizations', 'AuthorizationsController@weappStore')
-         ->name('api.weapp.authorizations.store');
+   $api->post('authorizations','AuthorizationsController@store')->name('api.authorization.store');
+       //小程序登录接口
+   $api->post('weapp/authorizations', 'AuthorizationsController@weappStore')->name('api.weapp.authorizations.store');
+     //小程序注册
+   $pai->post('weapp/users','UsersController@weappStore')->name('api.weapp.users.store');
        //刷新token
-     $api->put('authorizations/current','AuthorizationsController@update')
-     ->name('api.authorizations.update');
+   $api->put('authorizations/current','AuthorizationsController@update')->name('api.authorizations.update');
        //删除token
-     $api->delete('authorizations/current','AuthorizationsController@destroy')
-     ->name('api.authorizations.destroy');
+   $api->delete('authorizations/current','AuthorizationsController@destroy')->name('api.authorizations.destroy');
 
        //游客可以访问的接口
-     $api->get('categories','CategoriesController@index')
-     ->name('api.categories.index');
-     $api->get('topics', 'TopicsController@index')
-     ->name('api.topics.index');
+   $api->get('categories','CategoriesController@index')
+   ->name('api.categories.index');
+   $api->get('topics', 'TopicsController@index')
+   ->name('api.topics.index');
        //某个用户发布的话题
-     $api->get('users/{user}/topics','TopicsController@userIndex')
-     ->name('api.users.topics.index');
-     $api->get('topics/{topic}','TopicsController@show')
-     ->name('api.topics.show');
+   $api->get('users/{user}/topics','TopicsController@userIndex')
+   ->name('api.users.topics.index');
+   $api->get('topics/{topic}','TopicsController@show')
+   ->name('api.topics.show');
        //话题回复列表
-     $api->get('topics/{topic}/replies','RepliesController@index')
-     ->name('api.topics.replies.index');
+   $api->get('topics/{topic}/replies','RepliesController@index')
+   ->name('api.topics.replies.index');
        // 某个用户的回复列表
-     $api->get('users/{user}/replies', 'RepliesController@userIndex')
-     ->name('api.users.replies.index');
+   $api->get('users/{user}/replies', 'RepliesController@userIndex')
+   ->name('api.users.replies.index');
        //资源推荐
-     $api->get('links','LinksController@index')
-     ->name('api.links.index');
+   $api->get('links','LinksController@index')
+   ->name('api.links.index');
        //活跃用户
-     $api->get('actived/users','UsersController@activedIndex')
-     ->name('api.actived.users.index');
+   $api->get('actived/users','UsersController@activedIndex')
+   ->name('api.actived.users.index');
 
 
        //需要token验证的接口
-     $api->group(['middleware' => 'api.auth'], function ($api){
+   $api->group(['middleware' => 'api.auth'], function ($api){
            //当前登录用户信息
-         $api->get('user','UsersController@me')
-         ->name('api.user.show');
+     $api->get('user','UsersController@me')
+     ->name('api.user.show');
            //编辑个人资料
             //put 替换某个资源，需提供完整的资源信息
             //patch 部分修改资源，提供部分资源信息
-         $api->patch('user','UsersController@update')
-         ->name('api.user.update');
+     $api->patch('user','UsersController@update')
+     ->name('api.user.update');
            // 图片资源
-         $api->post('images', 'ImagesController@store')
-         ->name('api.images.store');
+     $api->post('images', 'ImagesController@store')
+     ->name('api.images.store');
            //发表话题
-         $api->post('topics','TopicsController@store')
-         ->name('api.topics.store');
-         $api->patch('topics/{topic}','TopicsController@update')
-         ->name('api.topics.update');
-         $api->delete('topics/{topics}','TopicsController@destroy')
-         ->name('api.topics.destroy');
+     $api->post('topics','TopicsController@store')
+     ->name('api.topics.store');
+     $api->patch('topics/{topic}','TopicsController@update')
+     ->name('api.topics.update');
+     $api->delete('topics/{topics}','TopicsController@destroy')
+     ->name('api.topics.destroy');
 
            //  删除文章
-         $api->delete('topics/{topic}','TopicsController@destroy')
-         ->name('api.topics.destroy');
+     $api->delete('topics/{topic}','TopicsController@destroy')
+     ->name('api.topics.destroy');
            //发布回复
-         $api->post('topics/{topic}/replies','RepliesController@store')
-         ->name('api.topics.replies.store');
+     $api->post('topics/{topic}/replies','RepliesController@store')
+     ->name('api.topics.replies.store');
            //删除回复
-         $api->delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')
-         ->name('api.topics.replies.destroy');
+     $api->delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')
+     ->name('api.topics.replies.destroy');
            //通知列表
-         $api->get('user/notifications','NotificationsController@index')
-         ->name('api.user.notifications.index');
+     $api->get('user/notifications','NotificationsController@index')
+     ->name('api.user.notifications.index');
            //通知统计
-         $api->get('user/notifications/stats','NotificationsController@stats')
-         ->name('api.user.notifications.stats');
+     $api->get('user/notifications/stats','NotificationsController@stats')
+     ->name('api.user.notifications.stats');
            //标记消息通知为已读
-         $api->patch('user/read/notifications','NotificationsController@read')
-         ->name('api.user.notifications.read');
+     $api->patch('user/read/notifications','NotificationsController@read')
+     ->name('api.user.notifications.read');
            //当前登录用户权限
-         $api->get('user/permissions','PermissionsController@index')
-         ->name('api.user.permissions.index');
+     $api->get('user/permissions','PermissionsController@index')
+     ->name('api.user.permissions.index');
 
-     });
+   });
  });
 });
 
