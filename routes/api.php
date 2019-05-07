5ac7afbcce36532224c8cca9869abccb94189f47
vagrant <vagrant@homestead>
2019-05-07 01:42:28 +0000

    pages user show

--- routes/api.php
+++ routes/api.php
@@ -35,19 +35,17 @@ $api->version('v1', [
    $api->put('authorizations/current','AuthorizationsController@update')->name('api.authorizations.update');
        //删除token
    $api->delete('authorizations/current','AuthorizationsController@destroy')->name('api.authorizations.destroy');
+   // 用户详情
+         $api->get('users/{user}', 'UsersController@show')->name('api.users.show');
     // 小程序注册
    $api->post('weapp/users', 'UsersController@weappStore')->name('api.weapp.users.store');
 
        //游客可以访问的接口
-   $api->get('categories','CategoriesController@index')
-   ->name('api.categories.index');
-   $api->get('topics', 'TopicsController@index')
-   ->name('api.topics.index');
+   $api->get('categories','CategoriesController@index')->name('api.categories.index');
+   $api->get('topics', 'TopicsController@index')->name('api.topics.index');
        //某个用户发布的话题
-   $api->get('users/{user}/topics','TopicsController@userIndex')
-   ->name('api.users.topics.index');
-   $api->get('topics/{topic}','TopicsController@show')
-   ->name('api.topics.show');
+   $api->get('users/{user}/topics','TopicsController@userIndex')->name('api.users.topics.index');
+   $api->get('topics/{topic}','TopicsController@show')->name('api.topics.show');
        //话题回复列表
    $api->get('topics/{topic}/replies','RepliesController@index')
    ->name('api.topics.replies.index');
